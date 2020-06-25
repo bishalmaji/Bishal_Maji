63d4c73e75a9ab197c84264cc6197e3e03f9e4f3
bishalmaji <67412398+bishalmaji@users.noreply.github.com>
2020-06-25 15:37:39 +0530

    Add files via upload

---
+++ pages/sampleweb.js
@@ -0,0 +1,23 @@
+function fun1(){
+    var user=document.getElementById("username_id").value;
+regx =new RegExp("E00","i");
+if (regx.test(user)) {
+    alert("valid username ") ;  
+    document.getElementById("lable").style.visibility="hidden";
+}
+else {
+    alert("not a valid user name");
+    document.getElementById("lable").style.visibility="visible";
+}
+}
+var ID=0;
+
+function changeword(){
+    document.getElementById("opid").innerHTML="4 seconds have already passed";
+}
+function start(){
+ alert("hello"); 
+}
+function stop(){
+
+}
